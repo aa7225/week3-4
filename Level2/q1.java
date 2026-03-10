de3de552e8dd9cd89a9994f3b517f8a15cb80ba9
Aadhira <aadhirakshu@gmail.com>
2026-03-10 13:33:23 +0530

    Level2 Question 2 solution

--- Level2/q1.java
+++ Level2/q1.java
@@ -7,11 +7,10 @@ public class q1 {
         System.out.print("Enter a number: ");
         int number = scanner.nextInt();
 
-        // Check for Natural Number (positive integer)
         if (number <= 0) {
             System.out.println("Please enter a natural number (greater than 0).");
         } else {
-            // Iterate from 1 to the entered number
+           
             for (int i = 1; i <= number; i++) {
                 if (i % 2 == 0) {
                     System.out.println(i + " is an even number");
